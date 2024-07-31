8998babfc04446d91a4ea2c9e3222301f2c27fda
Pimdyw <125757525+pimdyw@users.noreply.github.com>
2024-07-31 17:07:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'PDW';
+  return (
+    <div>
+      Hello {value}
+      <img src="https://media.discordapp.net/attachments/725983079675461725/1216755952548839555/IMG_20240130_154639_754.webp?ex=66aaf409&is=66a9a289&hm=289edce02710372532097c0bc529d92f4c28be348b2b0947a5a14713cbe459d9&=&format=webp&width=412&height=733" />
+    </div>
+  );
 }
 
 export default App;
